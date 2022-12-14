253b4e2fc6eb72c042c1fec6e5ea253fe1972f34
julianatavora <55543540+julianatavora@users.noreply.github.com>
2022-12-14 16:30:23 +0100

    Update conv_example.m

--- spectral_conv/conv_example.m
+++ spectral_conv/conv_example.m
@@ -10,7 +10,7 @@ clear all; close all; clc
 %                              2. SRF from satellite sensor
 %                              3. in-situ radiometric Lw and Ed data and wavelenghts
 %
-% Outputs: Rrs spectrally conv. for Sentinel 2 A bands, to apply for Sentinel 2B change SRF and central bands
+% Outputs: Rrs spectrally conv. 
 %
 %
 % In-situ data used here is available from the SeaSWIR dataset:  https://doi.pangaea.de/10.1594/PANGAEA.886287
@@ -31,9 +31,15 @@ Ed = [nm; Ed]'; clear nm
 %% prepare in-situ radiometric data
 
 
-SRF_centralband  = 'MSI_centralBands.m';
+SRF_centralband  = [442.7 492.7 559.8 664.6 704.1 740.5 782.8 832.8 864.7 945.1 1373.5 1613.7 2202.4];
 SRF_curve        = 'MSI_A_SRF.m';
 
 
 
-[Rrs_conv, central_band] = conv2sat(SRF_centralband,SRF_curve,Lw, Ed)
+[Rrs_conv, central_band] = conv2sat(SRF_centralband,SRF_curve,Lw, Ed);
+
+plot(nm_insitu,Lw(:,2)./Ed(:,2))
+hold on
+plot(central_band(SameBands),Rrs_conv(1,:),'o')
+
+
